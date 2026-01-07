2d6e58f8fee9a0b26a88a049622429ca24c65029
jim2107054 <mdjahid205hasan@gmail.com>
2026-01-07 16:53:05 +0600

    refactor: clean up layout component formatting for better readability

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,16 +5,16 @@ import SafeScreen from 'components/SafeScreen';
 
 export default function Layout() {
   return (
-    <SafeAreaProvider >
-    <SafeScreen>
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="onboarding1" />
-      <Stack.Screen name="onboarding2" />
-      <Stack.Screen name="onboarding3" />
-      <Stack.Screen name="login" />
-    </Stack>
-    </SafeScreen>
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="onboarding1" />
+          <Stack.Screen name="onboarding2" />
+          <Stack.Screen name="onboarding3" />
+          <Stack.Screen name="login" />
+        </Stack>
+      </SafeScreen>
     </SafeAreaProvider>
   );
 }
