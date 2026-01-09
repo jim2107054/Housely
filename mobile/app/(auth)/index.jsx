def73e165111855247c08f01362ab655f0fefe6e
jim2107054 <mdjahid205hasan@gmail.com>
2026-01-09 11:31:20 +0600

    feat: add Remember Me functionality and navigation enhancements to login screen

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -13,11 +13,13 @@ import { Link, useRouter } from "expo-router";
 import Toast from "react-native-toast-message";
 import useAuthStore from "../../store/authStore";
 import { ActivityIndicator } from "react-native-paper";
+import { ArrowLeft } from "lucide-react-native";
 
 const index = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [isRemembered, setIsRemembered] = useState(false);
 
   const { isLoading, login } = useAuthStore();
   const router = useRouter();
@@ -26,29 +28,33 @@ const index = () => {
     // Implement login logic here
     const result = await login(email, password);
 
-    if(!result.success){
+    if (!result.success) {
       Toast.show({
-        type: 'error',
-        text1: 'Login Failed',
-        text2: result.message || 'An error occurred during login',
-        position: 'top',
+        type: "error",
+        text1: "Login Failed",
+        text2: result.message || "An error occurred during login",
+        position: "top",
         visibilityTime: 4000,
       });
-    }
-    else{
+    } else {
       Toast.show({
-        type: 'success',
-        text1: 'Success!',
-        text2: 'Logged in successfully',
-        position: 'top',
+        type: "success",
+        text1: "Success!",
+        text2: "Logged in successfully",
+        position: "top",
         visibilityTime: 3000,
-      })
+      });
     }
 
     // On successful login, you might want to navigate to the main app screen
-    if(result.success){
-      router.replace('/(tabs)'); // Home screen after login
+    if (result.success) {
+      router.replace("/(tabs)"); // Home screen after login
     }
+  };
+
+  const RememberMeHandler = () => {
+    // Implement Remember Me functionality here
+    setIsRemembered(!isRemembered);
   };
 
   return (
@@ -56,15 +62,23 @@ const index = () => {
       className="flex-1"
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
-      <View className="flex-1 justify-center items-center bg-primary/10">
-        <Image
-          source={require("../../assets/images/Bookshop-bro.png")}
-          className="max-h-52 max-w-52 mb-4"
-        />
-        <View className="w-11/12 bg-cardBackground p-4 rounded-md shadow-md">
+      <View className="flex-1 bg-cardBackground justify-center items-center">
+        <View className="w-full h-full py-6 px-4">
+          <TouchableOpacity className="mb-6">
+            <ArrowLeft size={24} color="black" onPress={() => router.back()} />
+          </TouchableOpacity>
+          <View>
+            <Text className="text-start text-3xl font-bold text-gray-900 mb-2">
+              Welcome Back !
+            </Text>
+            <Text className="text-start font-semibold text-gray-500 mb-6">
+              Sign in with your email and password {"\n"}
+              or social media to continue
+            </Text>
+          </View>
           {/* Email Input Field */}
           <View className="mb-4">
-            <Text className="text-lg font-bold text-start text-gray-500 mb-1">
+            <Text className="text-lg font-bold text-start text-gray-800 mb-1">
               Email
             </Text>
             <View className="relative">
@@ -86,7 +100,7 @@ const index = () => {
           </View>
           {/* Password Input Field */}
           <View className="mb-4">
-            <Text className="text-lg font-bold text-start text-gray-500 mb-1">
+            <Text className="text-lg font-bold text-start text-gray-800 mb-1">
               Password
             </Text>
             <View className="relative">
@@ -115,25 +129,71 @@ const index = () => {
               </TouchableOpacity>
             </View>
           </View>
+          {/*Remember me & Forgot Password */}
+          <View className="flex-row justify-between items-center mb-6">
+            <TouchableOpacity
+              className="flex-row items-center"
+              onPress={RememberMeHandler}
+            >
+              <Ionicons
+                name={isRemembered ? "checkbox" : "square-outline"}
+                size={20}
+                color={isRemembered ? "#4F46E5" : "secondary"}
+                className="mr-2"
+              />
+              <Text className="text-gray-700 font-semibold">Remember me</Text>
+            </TouchableOpacity>
+            <Link href="/(auth)/forgot-password">
+              <Text className="text-secondary">Forgot Password?</Text>
+            </Link>
+          </View>
           {/* Login Button */}
           <TouchableOpacity
-            className="bg-primary rounded-lg py-3"
+            className="bg-secondary rounded-lg py-4"
             onPress={handleLogin}
           >
-            {
-              isLoading ? (
-                <ActivityIndicator animating={true} size="small" color="white" />
-              ) : (
-                <Text className="text-white text-center text-lg font-semibold">
-                  Log In
-                </Text>
-              )
-            }
+            {isLoading ? (
+              <ActivityIndicator animating={true} size="small" color="white" />
+            ) : (
+              <Text className="text-white text-center text-lg font-semibold">
+                Sign In
+              </Text>
+            )}
           </TouchableOpacity>
+          {/* FaceBook or Google Login */}
+          <View className="mt-6 flex-col justify-center items-center">
+            <View className="flex-row items-center mx-2">
+              <View className="h-px bg-gray-300 flex-1" />
+              <Text className="mx-2 text-lg text-gray-400">or</Text>
+              <View className="h-px bg-gray-300 flex-1" />
+            </View>
+            {/* social media login buttons */}
+            <View className="flex-row my-6 gap-5 justify-center items-center">
+              <TouchableOpacity className="mx-2 bg-gray-500/10 p-3 rounded-full">
+                <Image
+                  source={require("../../assets/images/facebook.png")}
+                  className="w-8 h-8"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity className="mx-2 bg-gray-500/10 p-3 rounded-full">
+                <Image
+                  source={require("../../assets/images/google.png")}
+                  className="w-8 h-8"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          {/* Sign Up Link */}
           <View className="mt-4 flex-row justify-center">
-            <Text className="text-gray-600">Don&apos;t have an account? </Text>
+            <Text className="text-gray-600 text-lg">
+              Don&apos;t have an account?{" "}
+            </Text>
             <Link href="/(auth)/signup">
-              <Text className="text-primary font-semibold">Sign Up</Text>
+              <Text className="text-secondary text-lg font-semibold">
+                Sign Up
+              </Text>
             </Link>
           </View>
         </View>
