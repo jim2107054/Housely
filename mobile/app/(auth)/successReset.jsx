d95136f9e816f1569acfeac035c8f84aae37dd57
jim2107054 <mdjahid205hasan@gmail.com>
2026-01-09 11:49:43 +0600

    feat: add successReset component for password reset success screen

---
+++ mobile/app/(auth)/successReset.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const successReset = () => {
+  return (
+    <View>
+      <Text>successReset</Text>
+    </View>
+  )
+}
+
+export default successReset
